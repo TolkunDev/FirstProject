070b5a59f6bdadbe3ed416ccb8c2afbf40dd456f
TolkunDev <165274091+TolkunDev@users.noreply.github.com>
2024-03-28 17:10:16 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+         int [] array = new int [length];
+         int minValue = 0;
+         int maxValue = 10;
+         // Заполнение массива случайными числами
+       for (int i = 0; i < length; i++){
+           array[i] = (int) (minValue + Math.random() * (maxValue - minValue + 1));
+       }
+        // Вывод массива
+        System.out.println("Ваш массив: ");
+       for (int i = 0; i < length; i++) {
+           System.out.print(array[i] + " ");
+       }
+        System.out.println();
+        for (int i = 0; i < array.length - 1; i++) {
+            for(int j = 0; j < array.length - i - 1; j++) {
+                if(array[j + 1] < array[j]) {
+                    int reserv = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = reserv;
+                }
+            }
+        }
+    for (int i = 0 ; i<length;i++) {
+        System.out.print(array[i] + " ");
+    }
+    }
+}
